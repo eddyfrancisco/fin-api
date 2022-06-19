2ed4de78f7c88dfe39fc59ed92d48c47bf49a4c6
Eddy Francisco <edgenilson.silva@sbkbs.com.br>
2022-06-19 15:03:20 -0300

    Até aula Middlewares

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -5,26 +6,41 @@ const app = express();
 
 const customers = [];
 
-app.listen(3333);
+function verifyIfExistsAccountCPF(request, response, next) {
+
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    };
+    
+    request.customer = customer;
+
+    return next();
+
+};
+
 
 app.use(express.json());
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    
+
     const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
     const id = uuidv4();
 
-    if(customerAlreadyExists){
+    if (customerAlreadyExists) {
 
-        return response.status(400).json({ error: "Customer already exists"});
+        return response.status(400).json({ error: "Customer already exists" });
     }
 
     customers.push({
-        cpf, 
-        name, 
-        id, 
+        cpf,
+        name,
+        id,
         statement: []
     });
 
@@ -32,4 +48,18 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send();
 
-})
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+
+    return response.json(customer.statement);
+
+});
+
+
+app.listen(3333);

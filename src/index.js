44ec64055cc855c916946c01c459a75a47a6f5b1
Eddy Francisco <edgenilson.silva@sbkbs.com.br>
2022-06-25 10:16:29 -0300

    Finaluzação módulo 'Primeiro Projeto com Node.js'

--- src/index.js
+++ src/index.js
@@ -15,13 +15,25 @@ function verifyIfExistsAccountCPF(request, response, next) {
     if (!customer) {
         return response.status(400).json({ error: "Customer not found" });
     };
-    
+
     request.customer = customer;
 
     return next();
 
 };
 
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
 
 app.use(express.json());
 
@@ -61,5 +73,85 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementoperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementoperation);
+
+    return response.status(201).send();
+
+});
+
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    };
+
+    const statementoperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementoperation);
+
+    return response.status(201).send();
+
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) =>
+            statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+    return response.json(statement);
+
+});
+
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+
+});
+
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } =  request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+
+});
 
 app.listen(3333);
